2dcdbff8eead33005168b02d756d6e046a3f62e0
wind <wind@localhost.localdomain>
2018-06-21 00:51:56 +0800

    init

--- centos7-jdk8-maven3/docker.sh
+++ centos7-jdk8-maven3/docker.sh
@@ -1,5 +1,5 @@
-docker rm -f centos7_jdk8_maven3
-docker rmi centos7-jdk8-maven3:7.8.3
-docker build -t centos7-jdk8-maven3:7.8.3 -f centos7_jdk8_maven3.dockerfile .
-docker run --privileged -t -d --name centos7_jdk8_maven3 centos7-jdk8-maven3:7.8.3 /usr/sbin/init;
-# docker exec -it centos7_jdk8_maven3 /bin/bash
+docker rm -f centos_jdk_maven
+docker rmi centos-jdk-maven:7.8.3
+docker build -t centos-jdk-maven:7.8.3 -f centos_jdk_maven.dockerfile .
+docker run --privileged -t -d --name centos_jdk_maven centos-jdk-maven:7.8.3 /usr/sbin/init;
+# docker exec -it centos_jdk_maven /bin/bash
