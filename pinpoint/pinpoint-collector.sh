c977478db9c46896ce2218c2fa9a140655fcbb79
qianchun <qianchun@huli.com>
2018-06-23 18:02:27 +0800

    init

--- pinpoint/pinpoint-collector.sh
+++ pinpoint/pinpoint-collector.sh
@@ -16,10 +16,10 @@ sed -i 's/port="8009"/port="18009"/g' /usr/local/collector/apache-tomcat-8.5.31/
 sed -i 's/redirectPort="8443"/redirectPort="18443"/g' /usr/local/collector/apache-tomcat-8.5.31/conf/server.xml
 #sed -i "s/localhost/`ifconfig eth0 | grep 'inet addr' | awk '{print $2}' | awk -F: '{print $2}'`/g"
 
-
 cd /opt/install/
 rm -rf /opt/install/pinpoint-collector-1.7.3.war
 rm -rf /usr/local/collector/apache-tomcat-8.5.31/webapps/ROOT/*
 wget https://github.com/naver/pinpoint/releases/download/1.7.3/pinpoint-collector-1.7.3.war
-unzip /opt/install/pinpoint-collector-1.5.2.war -d /usr/local/collector/apache-tomcat-8.5.31/webapps/ROOT
+unzip /opt/install/pinpoint-collector-1.7.3.war -d /usr/local/collector/apache-tomcat-8.5.31/webapps/ROOT
+sed -i 's/hbase.client.host=localhost/hbase.client.host=zookeeper/g'  /usr/local/collector/apache-tomcat-8.5.31/webapps/ROOT/WEB-INF/classes/hbase.properties
 echo "install pinpoint-collector finish"
--- pinpoint/pinpoint-web.sh
+++ pinpoint/pinpoint-web.sh
@@ -13,7 +13,8 @@ tar -zxvf apache-tomcat-8.5.31.tar.gz -C /usr/local/web/
 cd /opt/install/
 rm -rf /usr/local/web/apache-tomcat-8.5.31/webapps/ROOT/*
 wget https://github.com/naver/pinpoint/releases/download/1.7.3/pinpoint-web-1.7.3.war
-cp pinpoint-web-1.7.3.war /usr/local/tomcat/apache-tomcat-8.5.31/webapps/ROOT/
-
 unzip /opt/install/pinpoint-web-1.7.3.war -d /usr/local/web/apache-tomcat-8.5.31/webapps/ROOT
+
+
+sed -i 's/hbase.client.host=localhost/hbase.client.host=zookeeper/g'  /usr/local/web/apache-tomcat-8.5.31/webapps/ROOT/WEB-INF/classes/hbase.properties
 echo "install pinpoint-web finish"
